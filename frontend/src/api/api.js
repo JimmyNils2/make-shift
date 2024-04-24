cf8c2a0c68fed30f8ce540fd566298e01590b351
Darelis Quintero <jimmynils2@gmail.com>
2024-04-24 12:54:34 -0300

    Add backend URL

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,12 +2,12 @@ import axios from 'axios';
 import { ACCESS_TOKEN } from '../constants';
 import { getEnvVariables } from '../helpers';
 
-// const apiUrl = "https://b21e2f91-4330-4f59-be11-0c6d8ec6c1f2-dev.e1-us-cdp-2.choreoapis.dev/djangoreact/backend/rest-api-be2/v1.0"
+const apiUrl = "/choreo-apis/make-shift/backend/rest-api-be2/v1.0"
 
 const {VITE_API_URL} = getEnvVariables()
 
 const api = axios.create({
-    baseURL: VITE_API_URL /*? import.meta.env.VITE_API_URL : apiUrl*/
+    baseURL: VITE_API_URL ? VITE_API_URL : apiUrl
 });
 
 api.interceptors.request.use(
