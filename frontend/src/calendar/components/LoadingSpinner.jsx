1f31688fad6952db6357ab901ede91618d65bbb3
Darelis Quintero <jimmynils2@gmail.com>
2024-04-24 12:37:11 -0300

    LoadingSpinner Fixes

--- frontend/src/calendar/components/LoadingSpinner.jsx
+++ frontend/src/calendar/components/LoadingSpinner.jsx
@@ -1,9 +1,12 @@
-import { Spinner } from 'react-bootstrap';
-
+//import { Spinner } from 'react-bootstrap';
+import '../../styles/LoadingSpinner.css';
 export const LoadingSpinner = () => {
   return (
-    <div className="d-flex justify-content-center align-items-center min-vh-100">
-      <Spinner animation="border" role="status" style={{ color: '#347CF7' }} />
+    // <div className="d-flex justify-content-center align-items-center min-vh-100">
+    //   <Spinner animation="border" role="status" style={{ color: '#347CF7' }} />
+    // </div>
+    <div className="loading-container d-flex justify-content-center align-items-center min-vh-100">
+        <div className="loader"></div>
     </div>
   )
 }
